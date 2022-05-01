f15d1c683c666061eb4aaf1490c4153af405846e
Niklas Laurell <niklas.laurell@gmail.com>
2022-05-01 20:12:25 +0200

    Merge branch 'main' of https://github.com/NiklasLaurell/Website into main

--- Calender_Prototyp/Test_app/urls.py
+++ Calender_Prototyp/Test_app/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
-    path('Test_app/', views.index)  # domain Test_app.com
+    path('Test_app/', views.index),
+    path('Test_app/move', views.index2),  # domain Test_app.com
 ]
--- Calender_Prototyp/Test_app/views.py
+++ Calender_Prototyp/Test_app/views.py
@@ -5,24 +5,12 @@ from modules.calender_functions import get_matrix, get_time
 
 
 def index(request):
+    print(request)
     time = get_time()
     today = (time.year, time.month, time.day)
+    counter = 0
     matrix = get_matrix(time, 6, 7)
-    days = ['Måndag', 'Tisdag', 'Onsdag',
-            'Torsdag', 'Fredag', 'Lördag', 'Söndag']
-    return render(request, 'Test_app/index2.html', {
-        'days': days,
-        'matrix': matrix,
-        'time': time,
-        'today': today
-    })
 
-
-"""
-def index2(request):
-    time = get_time()
-    today = (time.year, time.month, time.day)
-    matrix = get_matrix(time, 6, 7)
     days = ['Måndag', 'Tisdag', 'Onsdag',
             'Torsdag', 'Fredag', 'Lördag', 'Söndag']
     return render(request, 'Test_app/index.html', {
@@ -31,4 +19,21 @@ def index2(request):
         'time': time,
         'today': today
     })
-"""
+
+
+def index2(request):
+    print(request)
+    time = get_time()
+    today = (time.year, time.month, time.day)
+
+    matrix = get_matrix(time, 8, 7)
+
+
+    days = ['Måndag', 'Tisdag', 'Onsdag',
+            'Torsdag', 'Fredag', 'Lördag', 'Söndag']
+    return render(request, 'Test_app/index.html', {
+        'days': days,
+        'matrix': matrix,
+        'time': time,
+        'today': today
+    })
